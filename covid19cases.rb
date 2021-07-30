8c684d20c26c3e34df3993ab60a1f9a02d4da086
Lich <liich@protonmail.com>
2021-07-30 06:27:35 +0000

    Add death count

--- covid19cases.rb
+++ covid19cases.rb
@@ -14,25 +14,27 @@ dates = { }
   doc.css('tr.mw-collapsible').map { |tr|
     date = tr.css('.bb-c:first').text()
     next if date[/\d{4}-\d{2}-\d{2}/].nil?
-    count = tr.css('td:nth-of-type(3)').text().gsub(',', '').to_i
+    count = tr.css('td:nth-of-type(3) span:first').text().gsub(',', '').to_i
+    deathCount = tr.css('td:nth-of-type(4) span:first').text().gsub(',', '').to_i
     dates[date] = { } if dates[date].nil?
-    dates[date][country] = [ count ]
-    data.push([ date, count ])
+    dates[date][country] = [ count, deathCount ]
+    data.push([ date, count, deathCount ])
   }
   data.each_with_index { |row, i|
-    new = 0
+    new = deathNew= 0
     new = data[i][1] - data[i-1][1] if i > 0
     dates[row[0]][country].push new
-    data[i][2] = new
+    deathNew = data[i][2] - data[i-1][2] if i > 0
+    dates[row[0]][country].push deathNew
   } 
 }
 
 dates.sort { |a, b| b[0] <=> a[0] }.to_h.each { |date, data|
   print "#{date}"
   data.each { |country, row|
-    print("\t#{country}: %7d, +%-6d" % row)
+    print("\t#{country}, C: %7d, D: %5d, %+7d, %+-5d" % row)
   }
   puts ""
 }
 
-puts "source code: https://gist.github.com/lccxz/c422faba53e6f82acdf0f595df82bddd"
+puts "\nsource code: https://gist.github.com/lccxz/c422faba53e6f82acdf0f595df82bddd"
