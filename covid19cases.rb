58b0a4c7b5daf78be892e44eb667c1c9e54009bc
Lich <liich@protonmail.com>
2021-08-01 22:37:29 +0800

    Add html out and country TH

--- covid19cases.rb
+++ covid19cases.rb
@@ -3,10 +3,13 @@
 require 'net/http'
 require 'nokogiri'
 
+SOURCE_CODE = "https://github.com/lccxz/covid19cases/blob/master/covid19cases.rb"
+
 dates = { }
 
 countries = { :US => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_States',
   :UK => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_United_Kingdom',
+  :TH => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Thailand',
   :PH => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_Philippines',
   :IN => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_India',
   :IL => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Israel',
@@ -31,15 +34,55 @@ countries = { :US => 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_the_Uni
   } 
 }
 
-dates.sort { |a, b| b[0] <=> a[0] }.to_h.each { |date, data|
-  print "#{date}"
-  countries.sort { |a, b| b[0] <=> a[0] }.to_h.each { |country, url|
-    print("  #{country}, ")
-    row = data[country]
-    print("C: %-8d D: %-7d %+-7d %+-6d" % row) if row
-    print("C: %-8s D: %-7s %+-7s %+-6s" % ([ '-' ] * 4)) if row.nil?
+def text_output(dates, countries)
+  dates.sort { |a, b| b[0] <=> a[0] }.to_h.each { |date, data|
+    print "#{date}"
+    countries.sort { |a, b| b[0] <=> a[0] }.to_h.each { |country, url|
+      print("  #{country}, ")
+      row = data[country]
+      print("C: %-8d D: %-7d %+-7d %+-6d" % row) if row
+      print("C: %-8s D: %-7s %+-7s %+-6s" % ([ '-' ] * 4)) if row.nil?
+    }
+    puts ""
   }
-  puts ""
-}
 
-puts "\nsource code: https://github.com/lccxz/covid19cases/blob/master/covid19cases.rb"
+  puts "\nsource code: #{SOURCE_CODE}"
+end
+
+def html_output(dates, countries)
+  puts '<doctype html>'
+  puts '<html>'
+  puts '<meta charset="utf-8" />'
+  puts '<title>COVID-19 cases table</title>'
+  puts ''
+  puts '<center><table style="text-align: center; border-spacing: 8px;">'
+  puts '<thead><tr><th><br/></th>'
+  countries = countries.sort { |a, b| b[0] <=> a[0] }.to_h.each { |country, url|
+    puts "<th colspan='2'>#{country}</th>"
+  }
+  puts '</tr><tr><th><br/></th>'
+  countries.each { |country, url|
+    puts '<th>Cases</th>'
+    puts '<th>Deaths</th>'
+  }
+  puts '</tr></thead>'
+  dates.sort { |a, b| b[0] <=> a[0] }.to_h.each { |date, data|
+    puts "<tr><td>#{date}</td>"
+    countries.each { |country, url|
+      row = data[country]
+      next puts("<td>-</td>" * 2) if row.nil?
+      puts "<td>#{row[0]} (+#{row[2]})</td><td>#{row[1]} (+#{row[3]})</td>"
+    }
+    puts "</tr>"
+  }
+  puts "</table><p>source code: <a href='#{SOURCE_CODE}' title='source code'>#{SOURCE_CODE}</a></center>"
+
+  return 0
+end
+
+
+if ARGV[0].upcase == "HTML"
+  html_output(dates, countries)
+else
+  text_output(dates, countries)
+end
